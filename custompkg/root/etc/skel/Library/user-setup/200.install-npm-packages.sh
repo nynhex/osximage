d25516c0783b81e2640fd353c89f41a4690fa3f9
Jeffrey Paul <sneak@sneak.berlin>
2016-10-07 20:58:38 -0400

    add npm package installer

---
+++ custompkg/root/etc/skel/Library/user-setup/200.install-npm-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set up paths and whatnot
+source $HOME/.profile
+
+NPM_PKGS="
+    coffee-script
+"
+
+for PKG in $NPM_PKGS ; do
+    # -g is okay because "global" prefix is where homebrew
+    # installed npm, i.e. in ~/Library/Homebrew, not actually systemwide
+    npm install -g $PKG
+done
