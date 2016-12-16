3e2954c67e5e83c525e5ef67a58c8db39eb089eb
Jeffrey Paul <sneak@acidhou.se>
2016-12-16 08:50:15 -0500

    added python3.5 binpath to path

--- custompkg/root/etc/skel/Library/bashrc.d/150.python-bin-path.sh
+++ custompkg/root/etc/skel/Library/bashrc.d/150.python-bin-path.sh
@@ -1,3 +1,7 @@
 if [[ -e $HOME/Library/Python/2.7/bin ]]; then
     PATH+=":$HOME/Library/Python/2.7/bin"
 fi
+
+if [[ -e $HOME/Library/Python/3.5/bin ]]; then
+    PATH+=":$HOME/Library/Python/3.5/bin"
+fi
