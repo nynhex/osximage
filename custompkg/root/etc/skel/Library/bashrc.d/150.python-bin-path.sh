4cdb6d82dff5241e084570c905ba89bd9724014c
Jeffrey Paul <sneak@acidhou.se>
2017-02-08 23:19:37 -0500

    Update 150.python-bin-path.sh

--- custompkg/root/etc/skel/Library/bashrc.d/150.python-bin-path.sh
+++ custompkg/root/etc/skel/Library/bashrc.d/150.python-bin-path.sh
@@ -2,6 +2,6 @@ if [[ -e $HOME/Library/Python/2.7/bin ]]; then
     PATH+=":$HOME/Library/Python/2.7/bin"
 fi
 
-if [[ -e $HOME/Library/Python/3.5/bin ]]; then
-    PATH+=":$HOME/Library/Python/3.5/bin"
+if [[ -e $HOME/Library/Python/3.6/bin ]]; then
+    PATH+=":$HOME/Library/Python/3.6/bin"
 fi
