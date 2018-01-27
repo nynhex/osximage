cf5cb4f8fa80421e609f7fe7ee1a6d0930a6f441
Jeffrey Paul <sneak@sneak.berlin>
2018-01-27 08:02:10 -0600

    move completion to profile from bashrc, update gdrive symlink dest

--- custompkg/root/etc/skel/Library/user-setup/900.link-googledrive.sh
+++ custompkg/root/etc/skel/Library/user-setup/900.link-googledrive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # setup google drive as synceddir in ~/.paths
 if [[ ! -e ~/.paths/synceddir ]]; then
-    ln -s ~/Google\ Drive ~/.paths/synceddir
+    ln -s ~/Google\ Drive\ File\ Stream/My\ Drive ~/.paths/synceddir
 fi
