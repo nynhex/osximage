df9e1a4430d4c7b982039eb0b3568f5eb74d8416
Jeffrey Paul <408977+sneak@users.noreply.github.com>
2018-09-11 02:00:27 -0700

    Create 200.google-cloud-sdk.sh

---
+++ custompkg/root/etc/skel/Library/bashrc.d/200.google-cloud-sdk.sh
@@ -0,0 +1,2 @@
+source "$HOME/Library/Homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
+source "$HOME/Library/Homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
