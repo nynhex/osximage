97e2c31a471ce13fccf6bf6471e1b46a992b2f14
Jeffrey Paul <jp@eeqj.com>
2018-10-25 05:09:48 -0700

    fix slow bash startup from overuse of brew --prefix

--- custompkg/root/etc/skel/Library/bashrc.d/200.git-prompt.sh
+++ custompkg/root/etc/skel/Library/bashrc.d/200.git-prompt.sh
@@ -1,5 +1,7 @@
-if [ -f "$(brew --prefix bash-git-prompt)/share/gitprompt.sh" ]; then
+BGPPREFIX="$HOME/Library/Homebrew/opt/bash-git-prompt"
+
+if [ -f "$BGPPREFIX/share/gitprompt.sh" ]; then
   GIT_PROMPT_THEME=Default
   GIT_PROMPT_ONLY_IN_REPO=1
-  source "$(brew --prefix bash-git-prompt)/share/gitprompt.sh"
+  source "$BGPPREFIX/share/gitprompt.sh"
 fi
