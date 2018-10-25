72dd27c3e4e62450698e215c8807bdd7637d318c
Jeffrey Paul <jp@eeqj.com>
2018-10-25 06:28:39 -0700

    update git-prompt theme, disable remote fetching

--- custompkg/root/etc/skel/Library/bashrc.d/200.git-prompt.sh
+++ custompkg/root/etc/skel/Library/bashrc.d/200.git-prompt.sh
@@ -1,7 +1,8 @@
 BGPPREFIX="$HOME/Library/Homebrew/opt/bash-git-prompt"
 
 if [ -f "$BGPPREFIX/share/gitprompt.sh" ]; then
-  GIT_PROMPT_THEME=Default
+  GIT_PROMPT_THEME=Plague_Doctor
   GIT_PROMPT_ONLY_IN_REPO=1
+  GIT_PROMPT_FETCH_REMOTE_STATUS=0
   source "$BGPPREFIX/share/gitprompt.sh"
 fi
